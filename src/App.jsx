1ab26f5955f32da49e1496a4d74872e12c4826bb
Ignacio <iggyjurado45@gmail.com>
2024-12-30 17:19:01 -0500

    Fixed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import SignUpForm from './Components/SignUpForm'
+import Authenticate from './Components/Authenticate'
+import { useState } from 'react';
+import './App.css'
+
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  return (
+    <>
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
+    </>
+  );
+}
+
+export default App
